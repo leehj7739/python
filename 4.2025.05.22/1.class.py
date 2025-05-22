1632f1052aec9494d5518907c7e6e263e4023983
heejoon lee <leehj7739@gmail.com>
2025-05-22 17:54:57 +0900

    20250522 과제

--- 4.2025.05.22/1.class.py
+++ 4.2025.05.22/1.class.py
@@ -66,15 +66,59 @@ class Library:
             print("해당하는 책이 없습니다.")
     
     #회원 등록
-    
-    #회원 수정
-    
+    def add_member(self, member):
+        self.members.append(member)
+        print(f"회원 등록: {member.id} , {member.name}")
+
     #회원 삭제
+    def del_member(self,member):
+        if len(self.members) == 0:
+            print("회원이 존재하지 않습니다.")
+            return
         
+        delmember = f"회원 삭제: {member.id} , {member.name}"
+        self.members.remove(member)
+        print(delmember)
+    
+    #회원 목록
+    def list_members(self):
+        if len(self.members) == 0:
+            print("회원이 존재하지 않습니다.")
+            return
+        
+        for member in self.members:
+            print(f"회원 목록: {member.id} , {member.name}")
+    
+    #회원별 대출 현황
+    def get_borrowed_books(self, member):
+        if len(self.members) == 0:
+            print("회원이 존재하지 않습니다.")
+            return
+        
+        if member in self.members:
+            print(f"회원 목록: {member.id} , {member.name}")
+            for book in member.borrowed_books:
+                print(f"대출 도서: {book.subject} , {book.author} , {book.isbn} , {book.year}")
+            
     #대출
+    def borrow_book(self, member, book):
+        if book not in self.books:
+            print("대출 가능한 도서가 없습니다.")
+            return
+        
+        member.borrowed_books.append(book)
+        self.books.remove(book)
+        print(f"대출자 {member.name} 대출 도서: {book.subject} , {book.author} , {book.isbn} , {book.year}")
     
     #반납
-    
+    def return_book(self, member, book):
+        if book not in member.borrowed_books:
+            print("반납 가능한 도서가 없습니다.")
+            return
+        
+        member.borrowed_books.remove(book)
+        self.books.append(book)
+        print(f"반납자 {member.name} 반납 도서: {book.subject} , {book.author} , {book.isbn} , {book.year}")
     
 
 
@@ -87,23 +131,33 @@ class Member:
         self.borrowed_books = []
 
 
-    #회원별 대출 현황 확인
-    def get_borrowed_books(self):
-        return self.borrowed_books
-
 
 
 library = Library()
-boo1 = Book("책1", "저자1", "ISBN1", "2025")
-
-library.add_book(boo1)
-
+b1 = Book("책1", "저자1", "ISBN1", "2025")
+b2 = Book("책2", "저자2", "ISBN2", "2026")
+b3 = Book("책3", "저자3", "ISBN3", "2027")
+library.add_book(b1)
+library.add_book(b2)
+library.add_book(b3)
 library.search_book("subject", "책1")
 
-library.search_book("subject", "책2")
 
-library.delete_book(boo1)
+member1 = Member("1", "홍길동")
+member2 = Member("2", "이순신")
+member3 = Member("3", "임꺽정")
+library.add_member(member1)
+library.add_member(member2)
+library.add_member(member3)
+library.list_members()
 
-library.search_book("subject", "책1")
+library.borrow_book(member1, b1)
+library.borrow_book(member1, b2)
+library.borrow_book(member1, b3)
+library.borrow_book(member2, b1)
+library.return_book(member1, b1)
+library.borrow_book(member2, b1)
+library.get_borrowed_books(member1)
+library.get_borrowed_books(member2)
 
 
